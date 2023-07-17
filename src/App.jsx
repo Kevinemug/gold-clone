59fef083fb1e26939cb7d6a46ce0944fc0d15998
Kevinemug <kevineeuwamahoro@gmail.com>
2023-07-17 15:45:07 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
     </div>
   </div>
   </header>    
-<main>
+<main className='bg '>
   {/* mobile */}
 
-  <div className='' >
+  <div className='bg-[#f6f7f7]' >
 <img src="https://www.gold.ac.uk/media/images-by-section/study/clearing-and-adjustment/clearing-2023/clearing-homepage-1-compressed.png" alt="" className='w-full'/>
 <div className='z-10 flex flex-col  justify-center items-center relative top-[-60px] px-7 lg:px-0 lg:w-[50%] lg:top-[-320px] lg:left-[30%]'>
   <img src="https://www.gold.ac.uk/media/images-by-section/study/clearing-and-adjustment/clearing-2023/CLEARED[79].svg" alt="" className=' ' />
@@ -34,14 +34,14 @@ function App() {
 </div>
 
 
-<div className=' bg-[#f6f7f7] p-4  lg:w-full lg:relative  top-[-300px]  lg:flex justify-end   lg:pr-20 '>
+<div className=' bg-[#f6f7f7] p-4  lg:w-full lg:relative  top-[-300px]  lg:flex justify-end   lg:pr-20  lg:hidden '>
 
 
 
 
 
 
-<div  className='hidden lg:block lg:absolute left-10 top-20'>
+<div  className='hidden  lg:block lg:absolute left-10 top-20'>
   <div>
     <img src="https://www.gold.ac.uk/media/images-by-section/homepage/2023/homepage_hero_march_23.png" alt="" />
   </div>
@@ -156,10 +156,147 @@ function App() {
 
 
 
+
+{/* TODO:2 */}
+
+
+
+
+<div  className=' lg:absolute left-10 mt-10 lg:mt-[430px] lg:ml-[50%] lg:hidden'>
+  <div>
+    <img src="https://www.gold.ac.uk/media/images-by-section/course-finder/short-courses/Art-Psychotherapy-shows_-2015_11156.jpg" alt="" />
+  </div>
+  {/* crd */}
+  <div  className='px-4 relative top-[-20px] lg:top-[-270px] lg:left-[300px]'>
+  <div className='bg-white py-10 lg:w-[50%]  '>
+    <div className='flex gap-5  flex-col justify-center items-center'>
+      <p className='text-black font-medium text-center p-2 text-xl'>Many of our short courses are available via live </p>
+      <p className='w-[70%] px-3 text-center lg:hidden'> Lorem ipsum Lorem ipsum dolor sit amet. dolor sit amet consectetur adipisicing elit. Porro, itaque!</p>
+    </div>
+<hr className='ml-6 mr-6 mt-10 py-4'/>
+<div className='flex justify-between px-6 pb-4'>
+  <p className='text-[#646ec0] font-medium'> Short courses</p>
+  <div>
+    <img src="https://www.gold.ac.uk/assets/img/svg/sprite/icon-more-666.svg" alt="" />
+  </div>
+</div>
+  </div>
+
+  </div>
+
 </div>
 
 
 
+
+
+
+</div>
+
+
+
+
+{/* TODO:grid */}
+<section className='bg-hero  bg-no-repeat h-[900px] mx-20 hidden bg-contain  lg:block '></section>
+<div className=' flex flex-col mx-20 '>
+  <section className='bg-input   hidden h-[200px] bg-no-repeat w-[80%]  bg-cover lg:block'></section>
+  <section className=' bg-grid bg-no-repeat h-[900px]  bg-contain hidden lg:block '>
+  
+  </section>
+</div>
+ 
+
+ <section className='cards grid gap-2 lg:grid-cols-4 w-[80%] m-auto'>
+
+<div className='lg:w-[200px] '>
+  <div className=''>
+    <img src="https://www.gold.ac.uk/media/images-by-section/about-us/news/IFUpic.jpg" alt="" />
+  </div>
+  <div className='flex justify-center items-center'>
+    <p className='w-[200px] text-[#376ebe] text-center'>Lorem ir, situr adipisicing elit. Facere a,</p>
+  </div>
+  <div className='flex justify-center  flex-col pt-5 items-center'>
+    < div className='flex gap-3 lg:hidden'>
+    <img src="https://www.gold.ac.uk/assets/img/svg/sprite/icon-calendar-999.svg" alt="" />
+    <div className='text-[#666666] '>Exhibition</div>
+    </div>
+    <div className='text-[#666666]'>18 ju 2023, 9am-9.00pm</div>
+  </div>
+</div>
+
+<div className='lg:w-[200px] '>
+  <div className=''>
+    <img src="https://www.gold.ac.uk/media/images-by-section/about-us/news/IFUpic.jpg" alt="" />
+  </div>
+  <div className='flex justify-center items-center'>
+    <p className='w-[200px] text-[#376ebe] text-center'>Lorem ir, situr adipisicing elit. Facere a,</p>
+  </div>
+  <div className='flex justify-center  flex-col pt-5 items-center'>
+    < div className='flex gap-3 lg:hidden'>
+    <img src="https://www.gold.ac.uk/assets/img/svg/sprite/icon-calendar-999.svg" alt="" />
+    <div className='text-[#666666] '>Exhibition</div>
+    </div>
+    <div className='text-[#666666]'>18 ju 2023, 9am-9.00pm</div>
+  </div>
+</div>
+
+<div className='lg:w-[200px] '>
+  <div className=''>
+    <img src="https://www.gold.ac.uk/media/images-by-section/about-us/news/IFUpic.jpg" alt="" />
+  </div>
+  <div className='flex justify-center items-center'>
+    <p className='w-[200px] text-[#376ebe] text-center'>Lorem ir, situr adipisicing elit. Facere a,</p>
+  </div>
+  <div className='flex justify-center  flex-col pt-5 items-center'>
+    < div className='flex gap-3 lg:hidden'>
+    <img src="https://www.gold.ac.uk/assets/img/svg/sprite/icon-calendar-999.svg" alt="" />
+    <div className='text-[#666666] '>Exhibition</div>
+    </div>
+    <div className='text-[#666666]'>18 ju 2023, 9am-9.00pm</div>
+  </div>
+</div>
+
+<div className='lg:w-[200px]  '>
+  <div className=''>
+    <img src="https://www.gold.ac.uk/media/images-by-section/about-us/news/IFUpic.jpg" alt="" />
+  </div>
+  <div className='flex justify-center items-center'>
+    <p className='w-[200px] text-[#376ebe] text-center'>Lorem ir, situr adipisicing elit. Facere a,</p>
+  </div>
+  <div className='flex justify-center  flex-col pt-5 items-center'>
+    < div className='flex gap-3 lg:hidden'>
+    <img src="https://www.gold.ac.uk/assets/img/svg/sprite/icon-calendar-999.svg" alt="" />
+    <div className='text-[#666666] '>Exhibition</div>
+    </div>
+    <div className='text-[#666666]'>18 ju 2023, 9am-9.00pm</div>
+  </div>
+</div>
+
+ </section>
+
+<section className='footer mt-5 bg-[#333333] font-bold grid grid-cols-2 p-8'>
+  <div className='text-[#f2ffff]'>
+<p>About us</p>
+<p>Contact us</p>
+<p>Find us</p>
+<p>Jobs</p>
+<p>Accessibility</p>
+<p>Statement</p>
+  </div>
+
+  <div className='text-[#f2ffff] '>
+    <p>Cookie use</p>
+<p>cpy</p>
+<p>disclianme</p>
+
+</div>
+ 
+<div>
+
+</div>
+</section>
+
+<div className='bg-roww bg-contain lg:hidden  m-auto bg-no-repeat  h-[200px]'></div>
 </main>
     
       </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+    'grid': "url('/src/assets/images/backgrid.PNG')",
+        'input': "url('/src/assets/images/input.PNG')",
+        'hero':"url('./src/assets/images//jjk.PNG')",
+        'roww':"url('./src/assets/images/loo.PNG')",
+      }
+    },
   },
   plugins: [],
 }
